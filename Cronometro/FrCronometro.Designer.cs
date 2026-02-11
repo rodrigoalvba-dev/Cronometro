93abdcddb2763e512cae2e43fddda2cff0920494
rodrigo <rodrigoalvba@gmail.com>
2026-02-10 20:03:07 -0600

    Agregar archivos de proyecto.

---
+++ Cronometro/FrCronometro.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Calculadora.Formularios
+{
+    partial class FrCronometro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Interval = 10;
+            timer1.Tick += timer1_Tick;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(224, 257);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "INICIAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(353, 257);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "DETENER";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(489, 257);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 2;
+            button3.Text = "REINICIAR";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Symbol", 48F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(163, 98);
+            label1.Name = "label1";
+            label1.Size = new Size(451, 106);
+            label1.TabIndex = 3;
+            label1.Text = "00:00:00,00";
+            label1.Click += label1_Click;
+            // 
+            // FrCronometro
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "FrCronometro";
+            Text = "FrCronometro";
+            Load += FrCronometro_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+    }
+}
---
+++ Cronometro/FrCronometro.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calculadora.Formularios
+{
+    public partial class FrCronometro : Form
+    {
+        int segundos = 0;
+int minutos = 0;
+int horas = 0;
+        int milisegundos=0;
+
+        public FrCronometro()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            milisegundos++;
+
+            if (milisegundos == 100)
+            {
+                milisegundos = 0;
+                segundos++;
+            }
+
+            if (segundos == 60)
+            {
+                segundos = 0;
+                minutos++;
+            }
+
+            if (minutos == 60)
+            {
+                minutos = 0;
+                horas++;
+            }
+            
+            label1.Text = horas.ToString("00") + ":" +
+                             minutos.ToString("00") + ":" +
+                             segundos.ToString("00")+milisegundos.ToString("00");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrCronometro_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            milisegundos = 0;
+            segundos = 0;
+            minutos = 0;
+            horas = 0;
+
+            label1.Text = "00:00:00,00";
+
+        }
+    }
+}
